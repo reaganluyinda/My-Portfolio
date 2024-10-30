a0940fc19cc07ec753e3e076cc6a70c4af13f26e
reagan luyinda <luyindareagan@gmail.com>
2024-10-30 17:26:15 +0300

    Initial commit
    environment setup and installing necessary technology

---
+++ reagan-luyinda/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-green-500">My Portfolio</h1>
+    </>
+  );
+}
+
+export default App;
