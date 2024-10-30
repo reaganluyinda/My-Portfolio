9304f1feaf055883d72bfda156c1b2abc2e8ce23
reagan luyinda <luyindareagan@gmail.com>
2024-10-30 23:47:35 +0300

    ref; imported the Hero component to App.tsx

--- reagan-luyinda/src/App.jsx
+++ reagan-luyinda/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/hero";
 
 function App() {
   return (
     <main className="mx-auto max-w-7xl overflow-x-hidden antialiased">
       <div className="bg-image fixed inset-0 bg-cover bg-fixed bg-center">
         <Navbar />
+        <Hero />
       </div>
     </main>
   );
