581fdbfcaf34d7e5d6854f3f6b02acc418e445b3
reagan luyinda <luyindareagan@gmail.com>
2024-10-31 11:26:58 +0300

    ref; customized a different div for the components

--- reagan-luyinda/src/App.jsx
+++ reagan-luyinda/src/App.jsx
@@ -6,7 +6,8 @@ import Projects from "./components/projects";
 function App() {
   return (
     <main className="mx-auto max-w-7xl overflow-x-hidden antialiased">
-      <div className="bg-image fixed inset-0 bg-cover bg-fixed bg-center">
+      <div className="bg-image fixed inset-0 bg-cover bg-fixed bg-center"></div>
+      <div className="relative z-10">
         <Navbar />
         <Hero />
         <Projects />
