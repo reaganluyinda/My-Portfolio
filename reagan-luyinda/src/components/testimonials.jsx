99556fe4f3106894d665449f90b6006a90061bf1
reagan luyinda <luyindareagan@gmail.com>
2024-11-05 17:32:50 +0300

    perf; Added animations to the testimonial section

--- reagan-luyinda/src/components/testimonials.jsx
+++ reagan-luyinda/src/components/testimonials.jsx
@@ -1,8 +1,56 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { TESTIMONIAL } from "../constants";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Testimonials = () => {
   const testimonialRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.from("#testimonial-image", {
+        opacity: 0,
+        scale: 0.8,
+        duration: 1,
+        ease: "power3.out",
+
+        scrollTrigger: {
+          trigger: testimonialRef.current,
+          start: "top 80%",
+          toggleActions: "play none none none",
+        },
+      });
+
+      gsap.from("#testimonial-quote", {
+        opacity: 0,
+        y: 30,
+        duration: 1,
+        ease: "power3.out",
+        delay: 0.3,
+        scrollTrigger: {
+          trigger: testimonialRef.current,
+          start: "top 80%",
+          toggleActions: "play none none none",
+        },
+      });
+
+      gsap.from("#testimonial-info", {
+        opacity: 0,
+        y: 20,
+        duration: 1,
+        ease: "power3.out",
+        stagger: 0.4,
+        scrollTrigger: {
+          trigger: testimonialRef.current,
+          start: "top 80%",
+          toggleActions: "play none none none",
+        },
+      });
+    }, testimonialRef);
+    return () => ctx.revert();
+  }, []);
   return (
     <section id="testimonial" className="py-16" ref={testimonialRef}>
       <h2 className="mb-8 text-center text-3xl font-medium lg:text-4xl">
@@ -14,11 +62,15 @@ const Testimonials = () => {
             src={TESTIMONIAL.image}
             alt={TESTIMONIAL.name}
             className="mb-6 h-24 w-24 rounded-full border-4 object-cover"
+            id="testimonial-image"
           />
-          <blockquote className="max-w-xl text-lg leading-relaxed lg:text-2xl">
+          <blockquote
+            id="testimonial-quote"
+            className="max-w-xl text-lg leading-relaxed lg:text-2xl"
+          >
             &quot;{TESTIMONIAL.quote}&quot;
           </blockquote>
-          <div className="mt-4 text-lg">
+          <div id="testimonial-info" className="mt-4 text-lg">
             <p>{TESTIMONIAL.name}</p>
             <p className="text-sm">{TESTIMONIAL.title}</p>
           </div>
